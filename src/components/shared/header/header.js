64c2c872bc64bbe3adbcedca2bf509568dac3fd8
Nataly Silva <mildred_natali@hotmail.com>
2020-03-31 11:39:22 -0600

    add Sass as plugin and continue working with the navbar

--- src/components/shared/header/header.js
+++ src/components/shared/header/header.js
@@ -1,9 +1,10 @@
-import { Link } from "gatsby"
-import React from "react"
+import { Link } from 'gatsby'
+import React from 'react'
+import './Header.scss'
 
 const Header = () => (
   <header>
-    <div className="d-flex align-items-center justify-content-center">
+    <div className="navbar d-flex align-items-center justify-content-center">
       <Link to="/" className="mr-2">Home</Link>
       <Link to="/" className="mr-2">About</Link>
       <Link to="/" className="mr-2">Experience</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import Image from '../components/image'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Welcome to my bio</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>I'm Mildred.</p>
     <p>Explore.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
