bf8a08ce8ef598c7bf0393ca3de89b98bafcca76
Mildred Silva M <mildred_natali@hotmail.com>
2020-04-16 17:12:17 -0500

    Adding header  (#1)
    Header component, bootstrap dependencies, icon for browser and installing bootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Portfolio web developer`,
-    description: `Look around my experience on the industry of the IT as front-end web developer.`,
-    author: `@gatsbyjs`,
+    title: `MILDRED SILVA`,
+    description: `Look around my experience in the industry of the IT as front-end web developer.`,
+    author: `mildred silva`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-resolve-src',
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -24,7 +26,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/components/shared/header/header.js
+++ src/components/shared/header/header.js
@@ -1,42 +1,34 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import Icon from 'images/icon.svg'
+import BurgerIcon from 'images/burger-icon.png'
+import './Header.scss'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h5 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          Home
-        </Link>
-      </h5>
-    </div>
-  </header>
-)
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  return (
+    <header className="navbar d-flex p-3">
+      <div className="d-md-flex w-100">
+        <div className="col-md-1 d-none d-md-flex justify-content-end p-0">
+          <img src={Icon} height="40" className="m-0 icon-navbar"/>
+        </div>
 
-Header.defaultProps = {
-  siteTitle: ``,
+        <div className="d-flex d-md-none w-100 justify-content-end">
+          <img src={BurgerIcon} height="30" value={isOpen} className="m-0" onClick={toggle}/>
+        </div>
+
+        <div className={`btn-menu d-md-flex align-items-center justify-content-center py-0 w-100 ${isOpen ? 'active' : ''}`}>
+          <Link to="#home" className="mr-md-4 my-3 my-md-0">Home</Link>
+          <Link to="#about" className="mr-md-4 my-3 my-md-0">About</Link>
+          <Link to="#experience" className="mr-md-4 my-3 my-md-0">Experience</Link>
+          <Link to="#skills" className="mr-md-4 my-3 my-md-0">Skills</Link>
+        </div>
+
+      </div>
+    </header>
+  )
 }
 
 export default Header
